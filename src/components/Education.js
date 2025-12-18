971ff1c3e420865f7b661ab8261735e7d34036de
T-Gobinath <gobinathemail@icloud.com>
2025-12-18 11:02:07 +0530

    Initial commit

--- src/components/Education.js
+++ src/components/Education.js
@@ -9,13 +9,13 @@ const Education = () => {
           <h3>Master of Computer Applications (MCA)</h3>
           <p className="period">2021</p>
           <p className="institution">Nandha Engineering College, Erode</p>
-          <p>CGPA: 8.1</p>
+          <p>CGPA: 8.01</p>
         </div>
         <div className="timeline-item">
           <h3>Bachelor of Computer Applications (BCA)</h3>
           <p className="period">2018</p>
           <p className="institution">Nandha Arts and Science College, Erode</p>
-          <p>CGPA: 6.1</p>
+          <p>CGPA: 6.4</p>
         </div>
       </div>
     </section>
